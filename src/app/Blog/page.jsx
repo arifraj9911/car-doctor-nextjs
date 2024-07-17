2458803d87e0c7d660cbc9c0ee05f11bb45e258f
Arif Hossain <arif.raj9911@gmail.com>
2024-07-17 22:24:28 +0600

    initial commit

---
+++ src/app/Blog/page.jsx
@@ -0,0 +1,12 @@
+import Blog from '@/components/Blog/Blog';
+import React from 'react';
+
+const BlogPage = () => {
+    return (
+        <div>
+            <Blog></Blog>
+        </div>
+    );
+};
+
+export default BlogPage;
